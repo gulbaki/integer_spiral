e35de88d7dbbe243bded9dc181d09ac45ad72d51
gulbaki <bbkgull@gmail.com>
2021-11-22 23:47:28 +0300

    go

--- core/database/DBConnector.php
+++ core/database/DBConnector.php
@@ -17,10 +17,11 @@ abstract class DBConnector
                // $dsn = sprintf('%s:host=%s;dbname=%s', 'mysql', 'localhost', 'integer_spiral');
                 //$db = new PDO($dsn, 'root', 'root');
 
-                $db->exec('set names utf8;');
-
                 $dsn = sprintf('%s:host=%s;dbname=%s', 'mysql', 'ulsq0qqx999wqz84.chr7pe7iynqr.eu-west-1.rds.amazonaws.com', 'khypvpajtdmcfvls');
                 $db = new PDO($dsn, 'mkzsohlx2kuz9kjj', 't113zyjlt0h0dxht');
+                $db->exec('set names utf8;');
+
+                
             } catch (PDOException $e) {
                 exit("Database fatal error!");
             }
