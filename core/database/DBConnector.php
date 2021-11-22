841467db58af7745cdf3f556932247fea31be7d2
gulbaki <bbkgull@gmail.com>
2021-11-23 00:19:23 +0300

    go

--- core/database/DBConnector.php
+++ core/database/DBConnector.php
@@ -14,19 +14,20 @@ abstract class DBConnector
         $db = &self::$pdo;
         if ($db === null) {
             try {
-                
+                $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+               
                // $dsn = sprintf('%s:host=%s;dbname=%s', 'mysql', 'localhost', 'integer_spiral');
                 //$db = new PDO($dsn, 'root', 'root');
-
-                $dsn = sprintf('%s:host=%s;dbname=%s', 'mysql', 'ulsq0qqx999wqz84.chr7pe7iynqr.eu-west-1.rds.amazonaws.com', 'khypvpajtdmcfvls');
-                $db = new PDO($dsn, 'mkzsohlx2kuz9kjj', 't113zyjlt0h0dxht');
+                 
+                $dsn = sprintf('%s:host=%s;dbname=%s', $url['schema'], $url['host'] , substr($url["path"], 1));
+                $db = new PDO($dsn, $url['user'], $url['pass']);
                 $db->exec('set names utf8;');
 
                 
             } catch (PDOException $e) {
-                $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+                
 
-                var_dump($url);
+             
                 exit;
                 exit("Database fatal error!");
             }
