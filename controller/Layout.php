92a3602d6c67cc7f38e050c493f8ac2634448777
gulbaki <bbkgull@gmail.com>
2021-11-22 23:11:58 +0300

    go

--- controller/Layout.php
+++ controller/Layout.php
@@ -49,7 +49,7 @@ class Layout extends Base
                 $createParams['col'] = $createParams['y'];
 
                 unset($createParams['x'], $createParams['y']);
-
+                $createParams['layout_matrix'] = html_entity_decode($createParams['layout_matrix']);
                 $insertId = $layoutService->create($createParams);
 
                 echo "layoutId: " . $insertId;
