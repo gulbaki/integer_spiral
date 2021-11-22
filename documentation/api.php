cf1545d14943d584f5ff40a1fff3abe14f0a62cf
gulbaki <bbkgull@gmail.com>
2021-11-22 22:31:35 +0300

    dump

--- documentation/api.php
+++ documentation/api.php
@@ -3,7 +3,11 @@
 include_once '../config.php';
 include_once("../vendor/autoload.php");
 
-$openapi = \OpenApi\Generator::scan(["/controller/Layout.php"] );
+$openapi = \OpenApi\Generator::scan(["/controller/Layout.php"]);
+
+print_r($openapi);
+exit;
+
 header('Content-Type: application/json');
  
 echo $openapi->toJSON(); 
