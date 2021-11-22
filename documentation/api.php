1e0728f386132eb67d1c7a7b28e226c6cae2f895
gulbaki <bbkgull@gmail.com>
2021-11-22 22:34:31 +0300

    go

--- documentation/api.php
+++ documentation/api.php
@@ -3,11 +3,12 @@
 include_once '../config.php';
 include_once("../vendor/autoload.php");
 
-$openapi = \OpenApi\Generator::scan(["/controller/Layout.php"]);
+$openapi = \OpenApi\Generator::scan(["/Applications/MAMP/htdocs/controller/Layout.php"] );
 
+
+echo "<pre>";
 print_r($openapi);
-exit;
-
+echo "</pre>";
 header('Content-Type: application/json');
  
 echo $openapi->toJSON(); 
