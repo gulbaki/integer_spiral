d91f81c038ee4b670270e046b6d74041806984ed
gulbaki <bbkgull@gmail.com>
2021-11-22 23:27:19 +0300

    go

--- core/database/DBDriver.php
+++ core/database/DBDriver.php
@@ -53,8 +53,7 @@ class DBDriver implements DBDriverInterface
 
         $sql = "INSERT INTO $table $keys VALUES $values;";
 
-        var_dump($params);
-        exit;
+     
         $statement = $this->db->prepare($sql);
         
         $statement->execute($params);
